094edce80f3cc287cb478cb6f5d2a0ed2be66d3d
Anshul889 <anshulmanaktala@gmail.com>
2021-01-10 10:02:19 +0530

    remove svg

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,61 +1,65 @@
 import React from 'react'
 import styled from 'styled-components'
+import home from '../../images/home.svg'
+
+const Wrapper = styled.div`
+height: 56px;
+display: grid;
+grid-template-columns: 1fr 1fr;
+text-transform: uppercase;
+width: 90%;
+margin: 0 auto;
+max-width: 1080px;
+place-content: center;
+@media (max-width: 480px) {
+  grid-template-columns: 1fr;
+  text-align: center;
+}
+`
+const Logo = styled.div`
+padding-left: 20px;
+@media (max-width: 480px) {
+  padding-left: 0;
+}
+`
+
+const DItems = styled.div`
+display: grid;
+grid-template-columns: repeat(5, 1fr);
+@media (max-width: 480px) {
+  display: none;
+}
+`
+
+const DItem = styled.div`
+display: grid;
+place-content: center;
+text-align: center;
+`
+
+const BottomNav = styled.div`
+bottom: 0;
+height: 56px;
+position: fixed;
+width: 100%;
+@media (min-width: 480px) {
+  display: none;
+}
+`
+
+const MItems = styled.div`
+display: grid;
+grid-template-columns: repeat(5, 1fr);
+`
+
+const MItem = styled.div`
+display: grid;
+grid-template-columns: 1fr;
+place-content: center;
+text-align: center;
+`
 
 const Header = ({ siteTitle, toggleTheme, theme }) => {
-  const Wrapper = styled.div`
-    height: 56px;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    text-transform: uppercase;
-    width: 90%;
-    margin: 0 auto;
-    max-width: 1080px;
-    place-content: center;
-    @media (max-width: 480px) {
-      grid-template-columns: 1fr;
-      text-align: center;
-    }
-  `
-  const Logo = styled.div`
-    padding-left: 20px;
-    @media (max-width: 480px) {
-      padding-left: 0;
-    }
-  `
-
-  const DItems = styled.div`
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    @media (max-width: 480px) {
-      display: none;
-    }
-  `
-
-  const DItem = styled.div`
-    display: grid;
-    place-content: center;
-    text-align: center;
-  `
-
-  const BottomNav = styled.div`
-    bottom: 0;
-    height: 56px;
-    @media (max-width: 480px) {
-      display: none;
-    }
-  `
-
-  const MItems = styled.div`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  `
-
-  const MItem = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  place-content: center;
-  text-align: center;
-  `
 
   return (
     <header>
@@ -73,11 +77,26 @@ const Header = ({ siteTitle, toggleTheme, theme }) => {
       </Wrapper>
       <BottomNav>
         <MItems>
-          <MItem></MItem>
-          <MItem></MItem>
-          <MItem></MItem>
-          <MItem></MItem>
-          <MItem></MItem>
+          <MItem>
+            <img src={home} />
+            <div>Home</div>
+          </MItem>
+          <MItem>
+            <img src={home} />
+            <div>Home</div>
+          </MItem>
+          <MItem>
+            <img src={home} />
+            <div>Home</div>
+          </MItem>
+          <MItem>
+            <img src={home} />
+            <div>Home</div>
+          </MItem>
+          <MItem>
+            <img src={home} />
+            <div>Home</div>
+          </MItem>
         </MItems>
       </BottomNav>
     </header>
