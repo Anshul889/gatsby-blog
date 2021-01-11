ced23dc7488104046fb2a7ba948a455d8f03b3d3
Anshul889 <anshulmanaktala@gmail.com>
2021-01-11 10:05:41 +0530

    fixed

--- src/components/useDarkMode.js
+++ src/components/useDarkMode.js
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 export const useDarkMode = () => {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState(
+    (typeof window !== 'undefined' && window.localStorage.getItem('theme')) ||
+      'light'
+  )
 
   const setMode = mode => {
     window.localStorage.setItem('theme', mode)
